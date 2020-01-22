640f032e46d5b518950e90c30ad767e4b298f993
Tavas  Neuwirth <tavasneuwirth@Tavass-MacBook-Pro.local>
2020-01-22 12:59:34 -0700

    working Calc

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TestCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a Operator + - * /: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another Number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
